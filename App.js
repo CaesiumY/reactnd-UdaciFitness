121a79d61cc47d5dfd1de431f642ff626de07efa
caesiumy <mn0658@naver.com>
2020-05-31 23:00:44 +0900

    feat: Add redux

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import AddEntry from "./components/AddEntry";
 import { View } from "react-native";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducers from "./reducer";
 
 export default function App() {
   return (
-    <View>
-      <AddEntry></AddEntry>
-    </View>
+    <Provider store={createStore(reducers)}>
+      <View>
+        <AddEntry></AddEntry>
+      </View>
+    </Provider>
   );
 }
