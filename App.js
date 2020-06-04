d5f3a1e4cc743c6976add1447b062ef17b78d66b
caesiumy <mn0658@naver.com>
2020-06-04 22:57:19 +0900

    style: Add tabBarIcon

--- App.js
+++ App.js
@@ -4,9 +4,11 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome } from "@expo/vector-icons";
 import reducers from "./reducer";
 import AddEntry from "./components/AddEntry";
 import History from "./components/History";
+import { purple, white, gray } from "./utils/colors";
 
 const Tab = createBottomTabNavigator();
 
@@ -16,7 +18,32 @@ export default function App() {
       <Provider store={createStore(reducers)}>
         <View style={{ flex: 1 }}>
           <View style={{ height: 20 }}></View>
-          <Tab.Navigator>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+
+                switch (route.name) {
+                  case "AddEntry":
+                    iconName = "plus";
+                    break;
+                  case "History":
+                    iconName = "history";
+                    break;
+                  default:
+                    iconName = "home";
+                    break;
+                }
+                return (
+                  <FontAwesome name={iconName} size={size} color={color} />
+                );
+              },
+            })}
+            tabBarOptions={{
+              activeTintColor: purple,
+              inactiveTintColor: gray,
+            }}
+          >
             <Tab.Screen name="AddEntry" component={AddEntry}></Tab.Screen>
             <Tab.Screen name="History" component={History}></Tab.Screen>
           </Tab.Navigator>
