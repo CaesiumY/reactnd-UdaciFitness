c3c01cbb407ac21de3c3e8790e699f5041997223
caesiumy <mn0658@naver.com>
2020-06-04 22:21:40 +0900

    feat: Add Tab Navigator

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
 import React from "react";
-import AddEntry from "./components/AddEntry";
-import History from "./components/History";
 import { View } from "react-native";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import reducers from "./reducer";
+import AddEntry from "./components/AddEntry";
+import History from "./components/History";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <Provider store={createStore(reducers)}>
-      <View style={{ flex: 1 }}>
-        {/* <AddEntry></AddEntry> */}
-        <View style={{ height: 20 }}></View>
-        <History></History>
-      </View>
-    </Provider>
+    <NavigationContainer>
+      <Provider store={createStore(reducers)}>
+        <View style={{ flex: 1 }}>
+          <View style={{ height: 20 }}></View>
+          <Tab.Navigator>
+            <Tab.Screen name="AddEntry" component={AddEntry}></Tab.Screen>
+            <Tab.Screen name="History" component={History}></Tab.Screen>
+          </Tab.Navigator>
+        </View>
+      </Provider>
+    </NavigationContainer>
   );
 }
