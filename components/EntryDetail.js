f0cb8a25c2549acadd13afe1004408c923af4158
caesiumy <mn0658@naver.com>
2020-06-06 19:38:31 +0900

    feat: Add reset button in entryDetail

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -3,13 +3,32 @@ import { Text, View, StyleSheet } from "react-native";
 import { connect } from "react-redux";
 import MetricCard from "./MetricCard";
 import { white } from "../utils/colors";
+import { addEntry } from "../actions";
+import { timeToString, getDailyReminderValue } from "../utils/helpers";
+import { removeEntry } from "../utils/api";
+import TextButton from "./TextButton";
 
 export class EntryDetail extends Component {
+  reset = () => {
+    const { goBack, remove, entryId } = this.props;
+
+    remove();
+    goBack();
+    removeEntry(entryId);
+  };
+
+  shouldComponentUpdate(nextProps) {
+    return nextProps.metrics !== null && !nextProps.metrics.today;
+  }
+
   render() {
     const { metrics } = this.props;
     return (
       <View style={styles.container}>
         <MetricCard metrics={metrics}></MetricCard>
+        <TextButton onPress={this.reset} style={{ margin: 20 }}>
+          RESET
+        </TextButton>
       </View>
     );
   }
@@ -31,4 +50,18 @@ function mapStateToProps(state, { route }) {
   };
 }
 
-export default connect(mapStateToProps)(EntryDetail);
+function mapDispatchToProps(dispatch, { route, navigation }) {
+  const { entryId } = route.params;
+  return {
+    goBack: () => navigation.goBack(),
+    remove: () =>
+      dispatch(
+        addEntry({
+          [entryId]:
+            timeToString() === entryId ? getDailyReminderValue() : null,
+        })
+      ),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EntryDetail);
