1b92d24d49f72d3f75a07aa1e4bfe2e60f0f024b
caesiumy <mn0658@naver.com>
2020-06-06 20:15:02 +0900

    feat: Add rendered by status in Live

--- components/Live.js
+++ components/Live.js
@@ -5,9 +5,30 @@ export class Live extends Component {
   state = {
     coords: null,
     status: null,
-    direction: null,
+    direction: "",
   };
   render() {
+    const { coords, status, direction } = this.state;
+    if (status === null) {
+      return <ActivityIndicator style={{ marginTop: 30 }}></ActivityIndicator>;
+    }
+
+    if (status === "denied") {
+      return (
+        <View>
+          <Text>denied</Text>
+        </View>
+      );
+    }
+
+    if (status === "undetermined") {
+      return (
+        <View>
+          <Text>undetermined</Text>
+        </View>
+      );
+    }
+
     return (
       <View>
         <Text> Live </Text>
