d88a9cafbbd658ffe03e2a5544b1720b581367c2
caesiumy <mn0658@naver.com>
2020-06-06 21:27:54 +0900

    feat: Add undetermined UI

--- components/Live.js
+++ components/Live.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
-import { Text, View, ActivityIndicator } from "react-native";
+import { Text, View, ActivityIndicator, StyleSheet } from "react-native";
+import { Location } from "expo";
+import { Foundation } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { purple, white } from "../utils/colors";
 
 export class Live extends Component {
   state = {
     coords: null,
-    status: null,
+    status: "undetermined",
     direction: "",
   };
+
+  askPermission = () => {};
   render() {
     const { coords, status, direction } = this.state;
     if (status === null) {
@@ -23,8 +29,12 @@ export class Live extends Component {
 
     if (status === "undetermined") {
       return (
-        <View>
-          <Text>undetermined</Text>
+        <View style={styles.center}>
+          <Foundation name="alert" size={50} />
+          <Text> You need to enable location services for this app.</Text>
+          <TouchableOpacity onPress={this.askPermission} style={styles.button}>
+            <Text style={styles.buttonText}>Enable</Text>
+          </TouchableOpacity>
         </View>
       );
     }
@@ -37,5 +47,30 @@ export class Live extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: purple,
+    alignSelf: "center",
+    borderRadius: 5,
+    margin: 20,
+  },
+  buttonText: {
+    color: white,
+    fontSize: 20,
+  },
+});
 
 export default Live;
